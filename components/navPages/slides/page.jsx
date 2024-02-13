72d5bcb11fb98e58a20ec7ab3aa3ae76b6c3ef26
Mohamed <elsebaey95@outlook.com>
2024-02-14 01:23:02 +0200

    add auto swiper

--- components/navPages/slides/page.jsx
+++ components/navPages/slides/page.jsx
@@ -3,7 +3,9 @@
 import Image from "next/image";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
+// import { Navigation, Pagination } from "swiper/modules";
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
 
 import { images } from "../../../lib/images";
 
@@ -11,16 +13,44 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
+
+
 export default function Page() {
   return (
     <section data-aos="zoom-in" data-aos-duration="500" className="py-12">
       <div className="container">
-        <Swiper
+        {/* <Swiper
           navigation
           pagination={{ type: "fraction" }}
           modules={[Navigation, Pagination]}
           // onSwiper={(swiper) => console.log(swiper)}
           className="h-96 w-full rounded-lg"
+        >
+          {images.map((image, index) => (
+            <SwiperSlide key={index}>
+              <div className="flex h-full w-full items-center justify-center">
+                <Image
+                  src={image.src}
+                  alt={image.alt}
+                  className="block h-full w-full object-cover"
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper> */}
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
         >
           {images.map((image, index) => (
             <SwiperSlide key={index}>
