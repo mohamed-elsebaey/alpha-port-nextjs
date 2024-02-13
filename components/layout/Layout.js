12f1d1ff7c77befab9795f2f1ce9ecdcc1cd4192
Mohamed <elsebaey95@outlook.com>
2024-02-13 23:31:26 +0200

    Delete Dark mode in Layout.js

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -3,7 +3,7 @@ import Footer from "../footer/Footer";
 
 function Layout(props) {
   return (
-    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-x-hidden">
+    <div className="bg-white duration-200 overflow-x-hidden">
       <MainNavigation />
       <main className="my-12 py-4">{props.children}</main>
       <Footer />
